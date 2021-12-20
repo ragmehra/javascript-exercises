9d5e803cb7b821b0801b11caed0cb696839704c8
Raghav Mehra <ragmehra@hotmail.com>
2021-12-20 10:39:23 -0800

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainList) {
+    let args = Array.from(arguments);
+    args.shift();
+
+    //Debugging code 1:
+    //console.log("Args: " + args);
+    //console.log("Main List: " + mainList);
+
+    for (let repeat = 0; repeat < args.length; repeat++)
+    {
+        for (const item of mainList ) {
+            if (args[repeat] === item) 
+            {
+                //Debugging code 2:
+                //console.log(item);
+                //console.log("Index of item to be removed: " + mainList.indexOf(item));
+                mainList.splice(mainList.indexOf(item), 1);
+            }
+        }
+    }
+
+    console.log("Final Main List: " + mainList);
+    return mainList;
 
 };
 
