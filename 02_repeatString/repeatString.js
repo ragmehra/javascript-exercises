a8a95695c372d8e937e99cc4789b0801d6ade41b
Raghav Mehra <ragmehra@hotmail.com>
2021-12-20 10:08:55 -0800

    Complete 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(repeatedString, numOfRepeats) {
+    let result = '';
 
+    if (numOfRepeats < 0) {
+        return "ERROR";
+    }
+
+    for (let repeats = 0; repeats < numOfRepeats; repeats++) {
+        result += repeatedString;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
