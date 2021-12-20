ca259f97459d65dc2bb9f1e107f6144c97185875
Raghav Mehra <ragmehra@hotmail.com>
2021-12-20 11:06:06 -0800

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(num) {
+  num = (num - 32) * (5/9);
+  num = Math.round(num * 10) / 10;
+  return num;
 
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  num = num * (9/5) + 32;
+  num = Math.round(num * 10) / 10;
+  return num;
 };
 
 // Do not edit below this line
