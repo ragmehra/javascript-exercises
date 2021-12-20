6a284e7c6872e7ac083d52e8e41feb44df758264
Raghav Mehra <ragmehra@hotmail.com>
2021-12-20 10:16:06 -0800

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
 const reverseString = function(str) {
+    let result = '';
+    let backwardsIndex = str.length -1;
 
+    for (let repeats = 0; repeats < str.length; repeats++) 
+    {
+        result += str[backwardsIndex];
+        backwardsIndex -= 1;
+    }
+
+    console.log(result);
+    return result;
 };
 
 // Do not edit below this line
