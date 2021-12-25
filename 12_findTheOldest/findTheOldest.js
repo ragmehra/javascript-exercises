52b8453c2833466ba1276eb816edd7649f766e1b
Raghav Mehra <ragmehra@hotmail.com>
2021-12-25 15:13:27 -0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+    let result = arr.sort((obj1, obj2) => {
+        let d = new Date;
+        if (!obj1.yearOfDeath) obj1.yearOfDeath = d.getFullYear();
+        if (!obj2.yearOfDeath) obj2.yearOfDeath = d.getFullYear();
+
+        let age1 = obj1.yearOfDeath - obj1.yearOfBirth;
+        let age2 = obj2.yearOfDeath - obj2.yearOfBirth;
+
+        return age1 > age2 ? -1 : 1;
+    
+    });
+
+    return result[0];
 
 };
 
