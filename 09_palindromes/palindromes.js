6333e9897e5a2c330cf76a1602ea3348e44f7177
Raghav Mehra <ragmehra@hotmail.com>
2021-12-25 14:23:54 -0800

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    text = text.toLowerCase();
+    text = text.replace(/\W/g, "");
+    console.log(text);
+
+    let testText = '';
+
+    for (let i = text.length - 1; i >= 0; i--) {
+        testText += text[i];
+    }
+
+    console.log(testText);
+
+    if (testText === text) return true;
+    
+    return false;
 
 };
 
