1992641779973e07aa6c27822ffcf760b0a89ff3
Raghav Mehra <ragmehra@hotmail.com>
2021-12-25 14:59:45 -0800

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let result =  [];
 
+    for (let obj of arr) {
+        result.push(obj.title);
+    }
+
+    console.log(result);
+
+    return result;
 };
 
 // Do not edit below this line
