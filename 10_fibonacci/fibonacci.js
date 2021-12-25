814bf576bf0481dddf01f6e7800d4646d9886ccb
Raghav Mehra <ragmehra@hotmail.com>
2021-12-25 14:52:40 -0800

    Complete Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index= Number(index);
+    if (index && index > 0) {
+        let end = 1; 
+        let previous = 1;
+        let previousHolder = previous;
 
+        if (index === 2) {
+            return end;
+        }
+
+        for (let i = 0; i < index - 1; i++)
+        {
+            previousHolder = end;
+            end = end + previous; 
+            previous = previousHolder;
+
+            //console.log(previous, end);
+        }
+        return previous;
+    }
+    return "OOPS";
 };
 
 // Do not edit below this line
